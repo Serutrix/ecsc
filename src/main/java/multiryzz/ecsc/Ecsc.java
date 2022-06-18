943a23573b93c817acd7f26447c2831fb2fcbfd1
Serutrix <kirbylp03@gmail.com>
2022-06-18 17:27:20 +0200

    yesss

---
+++ src/main/java/multiryzz/ecsc/Ecsc.java
@@ -0,0 +1,17 @@
+package multiryzz.ecsc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ecsc extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
